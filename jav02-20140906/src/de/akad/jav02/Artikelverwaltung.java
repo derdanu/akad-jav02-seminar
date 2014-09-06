c1bae1510f4605ad45c017278a2d0e22c0747562
Daniel Falkner <bmw@740i.de>
2014-09-06 10:39:12 +0200

    Hinzufügen von Artikeln

--- jav02-20140906/src/de/akad/jav02/Artikelverwaltung.java
+++ jav02-20140906/src/de/akad/jav02/Artikelverwaltung.java
@@ -1,4 +1,5 @@
 package de.akad.jav02;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -48,12 +49,50 @@ public class Artikelverwaltung {
 	}
 
 	public void addArtikel(String name, double ek, double vk) {
-		Artikel a = new Artikel();
-		a.setName(name);
-		a.setEk(ek);
-		a.setVk(vk);
 		
-		stamm.add(a);
+		
+		String query = "INSERT INTO artikel ("
+				    + " name,"
+				    + " ek,"
+				    + " vk"
+				    + " ) VALUES ("
+				    + "?, ?, ?)";
+
+		 try {
+		    // set all the preparedstatement parameters
+		    PreparedStatement st = this.db.getConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+		    st.setString(1, name);
+		    st.setDouble(2, ek);
+		    st.setDouble(3, vk);
+	
+	
+		    // execute the preparedstatement insert
+		    st.executeUpdate();
+		    ResultSet rs = st.getGeneratedKeys();
+		    int key = 0;
+		    if ( rs.next() ) {
+		        // Retrieve the auto generated key(s).
+		       key = rs.getInt(1);
+		    }
+		    
+			System.out.println("Id des neuen Datensatzes: " + key);
+	
+		    st.close();
+			
+			Artikel a = new Artikel();
+			a.setId(key);
+			a.setName(name);
+			a.setEk(ek);
+			a.setVk(vk);
+			
+			stamm.add(a);
+		  } 
+		  catch (SQLException e)
+		  {
+			e.printStackTrace();
+		  }
+
+		
 	}
 	
 	public void deleteArtikel(Artikel a) {
