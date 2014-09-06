8e76044553b16d45400cce3bc54b6f59d23b5e2a
Daniel Falkner <bmw@740i.de>
2014-09-06 18:25:35 +0200

    Abstand

--- jav02-20140906/src/de/akad/jav02/Artikel.java
+++ jav02-20140906/src/de/akad/jav02/Artikel.java
@@ -10,24 +10,31 @@ public class Artikel {
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public double getEk() {
 		return ek;
 	}
+	
 	public void setEk(double ek) {
 		this.ek = ek;
 	}
+	
 	public double getVk() {
 		return vk;
 	}
+	
 	public void setVk(double vk) {
 		this.vk = vk;
 	}
