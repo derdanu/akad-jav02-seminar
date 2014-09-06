2f25a7ce12525dc97ac3781dc656821834780a26
Daniel Falkner <bmw@740i.de>
2014-09-06 11:22:21 +0200

    Datenbank Klasse formatiert.

--- jav02-20140906/src/de/akad/jav02/Datenbank.java
+++ jav02-20140906/src/de/akad/jav02/Datenbank.java
@@ -8,29 +8,26 @@ public class Datenbank {
 
 	private Connection connect = null;
 
-	  // Eine (versteckte) Klassenvariable vom Typ der eigene Klasse
-	  private static Datenbank instance;
-	  // Verhindere die Erzeugung des Objektes über andere Methoden
-	  private Datenbank() {
+	// Eine (versteckte) Klassenvariable vom Typ der eigene Klasse
+	private static Datenbank instance;
+	// Verhindere die Erzeugung des Objektes über andere Methoden
+	private Datenbank() {
 		  
-			// this will load the MySQL driver, each DB has its own driver
-		    try {
-				Class.forName("com.mysql.jdbc.Driver");
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		    // setup the connection with the DB.
-		    try {
-				connect = DriverManager
-				      .getConnection("jdbc:mysql://127.0.0.1/jav02?"
-				          + "user=jav02&password=test");
-				System.out.println("Verbunden....");
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				//e.printStackTrace();
-				System.out.println("Fehler: " + e.getLocalizedMessage());
-			}
+		// this will load the MySQL driver, each DB has its own driver
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	    // setup the connection with the DB.
+	    try {
+	    	connect = DriverManager
+	    				.getConnection("jdbc:mysql://127.0.0.1/jav02?"
+	    							+ "user=jav02&password=test");
+			System.out.println("Verbunden....");
+		} catch (SQLException e) {
+			System.out.println("Fehler bei der Datenbankverdindung: " + e.getLocalizedMessage());
+		}
 		  
 	  }
 	  // Eine Zugriffsmethode auf Klassenebene, welches dir '''einmal''' ein konkretes 
@@ -38,18 +35,15 @@ public class Datenbank {
 	  // Durch 'synchronized' wird sichergestellt dass diese Methode nur von einem Thread 
 	  // zu einer Zeit durchlaufen wird. Der nächste Thread erhält immer eine komplett 
 	  // initialisierte Instanz.
-	  public static synchronized Datenbank getInstance () {
-	    if (Datenbank.instance == null) {
-	    	Datenbank.instance = new Datenbank ();
-	    }
-	    return Datenbank.instance;
-	  }
-	
-	
-	public Connection getConnection() {
-		
-		return connect;
+	public static synchronized Datenbank getInstance () {
+		if (Datenbank.instance == null) {
+			Datenbank.instance = new Datenbank ();
+		}
+		return Datenbank.instance;
 	}
-   
+	
+	public Connection getConnection() {	
+		return connect;
+	} 
 	
 }
