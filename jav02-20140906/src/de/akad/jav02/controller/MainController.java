68762f317c31d95b63898ae9bb1b4b094a3d214b
Daniel Falkner <bmw@740i.de>
2014-09-16 10:54:04 +0200

    Dokumentation

--- jav02-20140906/src/de/akad/jav02/controller/MainController.java
+++ jav02-20140906/src/de/akad/jav02/controller/MainController.java
@@ -3,11 +3,16 @@ package de.akad.jav02.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.List;
-
 import de.akad.jav02.model.Artikelverwaltung;
 import de.akad.jav02.view.MVCView;
 
+/**
+ * 
+ * Controller Klasse
+ * 
+ * @author dfalkner, egrinschuk
+ *
+ */
 public class MainController implements ActionListener {
 
 	Artikelverwaltung av;
@@ -15,12 +20,15 @@ public class MainController implements ActionListener {
 	
 	public MainController() {
 
+		// View Initialiseren
 		this.view = new MVCView(this);
 
+		// Model Initalisieren, Observer zuweisen
 		this.av = new Artikelverwaltung();
 		this.av.addObserver(this.view);
 		this.av.initDBData();
 		
+		// View Default Werte anzeigen
 		this.view.setSpinnerList(this.av.getDatenstamm());
 		this.view.setCBVector(this.av.getSpalten());
 		
@@ -31,43 +39,48 @@ public class MainController implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		
+		// Neuen Artikel hinzufuegen
 		if(arg0.getActionCommand().equals("add")){
 			 
-			 Double ek = Double.parseDouble(view.getEK().replace(",", "."));
-			 Double vk = Double.parseDouble(view.getVK().replace(",", "."));
+			Double ek = Double.parseDouble(view.getEK().replace(",", "."));
+			Double vk = Double.parseDouble(view.getVK().replace(",", "."));
 		 
-			 this.av.addArtikel(view.getName(), ek, vk);
-			 
-		 } else if (arg0.getActionCommand().equals("can")){
-			 
-			 view.setEK(0);
-			 view.setVK(0);
-			 view.setName("");
-			 
-		 } else if (arg0.getActionCommand().equals("del")) {
-			 
-			 this.av.deleteArtikel(view.getSpinnerArtikel());
-			 
-		 }	else if (arg0.getActionCommand().equals("del_liste")) {
-
-			 ArrayList<Integer> dbIds = new ArrayList<Integer>();
-
-			 // Bei mehrfach Auswahl alle ausgewaehlten Datenbank IDs holen 
-			 	
-			 for (int i: this.view.getTableSelectedRows()) {
-				 dbIds.add(this.view.TableGetDBValueAt(i));
-			 }
-				
-			 // Alle Artikel anhand der Datenbank ID entfernen
-			 for (int i: dbIds) {
-				this.av.deleteArtikel(i);
-			 }
+			this.av.addArtikel(view.getName(), ek, vk);
 		
-		 } else if (arg0.getActionCommand().equals("sort")) {
+		// Abbrechen, GUI Felder leeren
+		} else if (arg0.getActionCommand().equals("can")){
 			 
-			 this.av.sortTable(this.view.getCBSelectedItem());
+			view.setEK(0);
+			view.setVK(0);
+			view.setName("");
+
+		// Loeschen ueber JSpinner Auswahl
+		} else if (arg0.getActionCommand().equals("del")) {
 			 
-		 }
+			this.av.deleteArtikel(view.getSpinnerArtikel());
+		
+		// Loeschen ueber JTable Auswahl (auch Mehrfachauswahl)
+		} else if (arg0.getActionCommand().equals("del_liste")) {
+
+			ArrayList<Integer> dbIds = new ArrayList<Integer>();
+
+			// Bei mehrfach Auswahl alle ausgewaehlten Datenbank IDs holen 
+			 	
+			for (int i: this.view.getTableSelectedRows()) {
+				dbIds.add(this.view.TableGetDBValueAt(i));
+			}
+				
+			// Alle Artikel anhand der Datenbank ID entfernen
+			for (int i: dbIds) {
+				this.av.deleteArtikel(i);
+			}
+		
+		// Sortieren der JTable anhand der Datenbankspalten im Pulldown
+		} else if (arg0.getActionCommand().equals("sort")) {
+			 
+			this.av.sortTable(this.view.getCBSelectedItem());
+			 
+		}
 		
 	}
 
