6265569601e6a75792545f1308beab3c9fca16a2
Daniel Falkner <bmw@740i.de>
2014-09-16 10:57:38 +0200

    Dokumentation

--- jav02-20140906/src/de/akad/jav02/model/Artikel.java
+++ jav02-20140906/src/de/akad/jav02/model/Artikel.java
@@ -1,44 +1,109 @@
 package de.akad.jav02.model;
 
+/**
+ * 
+ * Artikel Klasse
+ * 
+ * @author dfalkner, egrinschuk
+ *
+ */
 public class Artikel {
 
+	// ID
 	private int id;
+	// Name
 	private String name;
+	// Einkaufspreis
 	private double ek;
+	// Verkaufspreis
 	private double vk;
 	
+	/**
+	 * 
+	 * Getter fuer ID
+	 * 
+	 * @return int
+	 */
 	public int getId() {
 		return id;
 	}
 	
+	/**
+	 * 
+	 * Setter fuer ID
+	 * 
+	 * @param id Int
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * 
+	 * Getter fuer Name
+	 * 
+	 * @return String;
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * 
+	 * Setter fuer Name
+	 * 
+	 * @param name String
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/**
+	 * 
+	 * Getter fuer Einkaufspreis
+	 * 
+	 * @return double
+	 */
 	public double getEk() {
 		return ek;
 	}
 	
+	/**
+	 * 
+	 * Setter fuer Einkaufspreis
+	 * 
+	 * @param ek double
+	 */
 	public void setEk(double ek) {
 		this.ek = ek;
 	}
 	
+	/**
+	 * 
+	 * Getter fuer Verkaufspreis
+	 * 
+	 * @return double
+	 */
 	public double getVk() {
 		return vk;
 	}
 	
+	/**
+	 * 
+	 * Setter fuer Verkaufspreis
+	 * 
+	 * @param vk double
+	 */
 	public void setVk(double vk) {
 		this.vk = vk;
 	}
 	
+	
+	/**
+	 * 
+	 * To String Methode
+	 * 
+	 */
 	public String toString() {
 		return this.name;
 	}
