de2a7b844a9a67e47dec3ba35dd8faecab29c3e4
Daniel Falkner <bmw@740i.de>
2014-09-16 11:05:20 +0200

    Dokumentation

--- jav02-20140906/src/de/akad/jav02/view/MVCViewInterface.java
+++ jav02-20140906/src/de/akad/jav02/view/MVCViewInterface.java
@@ -2,9 +2,16 @@ package de.akad.jav02.view;
 
 import java.util.List;
 import java.util.Vector;
-
 import de.akad.jav02.model.Artikel;
 
+/**
+ * 
+ * Interface fuer den View um die vom Controller 
+ * benoetigten Methoden implementieren zu muessen
+ * 
+ * @author dfalkner, egrinschuk
+ *
+ */
 public interface MVCViewInterface {
 
 	/**
@@ -31,17 +38,77 @@ public interface MVCViewInterface {
 	 */
 	public String getName();
 	
+	/**
+	 * 
+	 * Einkaufspreis setzen
+	 * 
+	 * @param ek double
+	 */
 	public void setEK(double ek);
+	
+	/**
+	 * 
+	 * Verkaufspreis setzen
+	 * 
+	 * @param vk double
+	 */
 	public void setVK(double vk);
+	
+	/**
+	 * 
+	 * Namen setzen
+	 * 
+	 * @param name String
+	 */
 	public void setName(String name);
 
+	/**
+	 * JSpinner Liste setzen
+	 * 
+	 * @param sl List<?>
+	 */
 	public void setSpinnerList(List<?> sl);
+	
+	/**
+	 * 
+	 * Ausgewaehlten Artikel im JSpinner zurueckgeben
+	 * 
+	 * @return Artikel
+	 */
 	public Artikel getSpinnerArtikel();
 	
+	/**
+	 * 
+	 * Ausgewaehlt Zeilen im JTable zurueckgeben
+	 * 
+	 * @return int[] 
+	 */
 	public int[] getTableSelectedRows();
+	
+	/**
+	 * 
+	 * Datenbank ID einer Zeile im JTable ermitteln
+	 * 
+	 * @param x int Zeile innerhalb des JTables
+	 * 
+	 * @return int DatenbankID
+	 */
 	public int TableGetDBValueAt(int x);
 	
+	/**
+	 * 
+	 * Ausgewaeltes Checkbox Feld zurueckgeben
+	 * 
+	 * @return String;
+	 */
 	public String getCBSelectedItem();
+	
+	/**
+	 * 
+	 * Checkbox Eintraege als Vector uebergeben
+	 * 
+	 * @param v Vector<String>
+	 */
 	public void setCBVector(Vector<String> v);
 
 		
